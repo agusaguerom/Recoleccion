4c841b574f30b7cf455cdb070889f4eac5c3ee54
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-04-26 17:11:15 -0300

    Update play.php

--- play.php
+++ play.php
@@ -8,25 +8,57 @@ require_once("Aldeano.php")
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
     <title>Play</title>
 </head>
 <body>
 
-<img class="fondo" src="img/Fondo.png" alt="fondo">
+
+ <header class= "headerplay">
+    <nav class="navbar navbar-light bg-light">
+    <form class="form-inline">
+        <button class="btn btn-outline-success" type="button">Recolectar</button>
+        <button class="btn btn-sm btn-outline-secondary" type="button">Comer</button>
+    </form>
+    </nav>
+  </header>
+  
 
 
-<?php
+  <main>
+
+    <section>
+    <?php
     echo " 
-    <div class=cardAldeano>
-    <h2 id=titnombrealdeano>Nombre</h2>
-    <p id=nombrealdeano> ". $Aldeano1->nombre ."</p>
-    <h2 id=idtitvelocidad>Velocidad De Recoleccion</h2>
-    <p id=aldeanorecoleccion>".  $Aldeano1->velocidadrecoleccion ."</p>
+    <div class='card text-white bg-info mb-3 carddatos'>
+    <div class='card-body'>
+      <h5 class='card-title'>Nombre</h5>
+      <p class='card-text'>" . $Aldeano1->nombre . "</p>
     </div>
     ";
 ?>
+    </section>
+
+    <section>
+    <?php
+    echo " 
+    <div class='card text-white bg-info mb-3 carddatos'>
+    <div class='card-body'>
+      <h5 class='card-title'>Vel. Recoleccion</h5>
+      <p class='card-text'>" . $Aldeano1->velocidadrecoleccion . "</p>
+    </div>
+    ";
+?>
+    </section>
 
 
+  </main>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 
