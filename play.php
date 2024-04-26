e724bcabe58661a50b07b94744f7faf84e9b7701
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-04-26 20:01:48 -0300

    Update play.php

--- play.php
+++ play.php
@@ -1,5 +1,6 @@
 <?php
-require_once("Aldeano.php")
+require_once("Aldeano.php");
+require_once("Arbusto.php");
 ?>
 
 <!DOCTYPE html>
@@ -7,60 +8,22 @@ require_once("Aldeano.php")
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-
-    <title>Play</title>
+    <title>Document</title>
 </head>
 <body>
-
-
- <header class= "headerplay">
-    <nav class="navbar navbar-light bg-light">
-    <form class="form-inline">
-        <button class="btn btn-outline-success" type="button">Recolectar</button>
-        <button class="btn btn-sm btn-outline-secondary" type="button">Comer</button>
-    </form>
-    </nav>
-  </header>
-  
-
-
-  <main>
-
-    <section>
-    <?php
-    echo " 
-    <div class='card text-white bg-info mb-3 carddatos'>
-    <div class='card-body'>
-      <h5 class='card-title'>Nombre</h5>
-      <p class='card-text'>" . $Aldeano1->nombre . "</p>
+    
+    <div class="row">
+        <div class="arbusto">
+            <img src="img/arbusto.png" alt="Arbusto imagen">
+        </div>
+        <div class="aldeano">
+            <img src="spritealdeano.png" alt="Aldeano Imagen">
+        </div>
+        <div class="arbusto">
+            <img src="img/Arbusto.png" alt="Arbusto imagen">
+        </div>
     </div>
-    ";
-?>
-    </section>
 
-    <section>
-    <?php
-    echo " 
-    <div class='card text-white bg-info mb-3 carddatos'>
-    <div class='card-body'>
-      <h5 class='card-title'>Vel. Recoleccion</h5>
-      <p class='card-text'>" . $Aldeano1->velocidadrecoleccion . "</p>
-    </div>
-    ";
-?>
-    </section>
-
-
-  </main>
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
-
 
