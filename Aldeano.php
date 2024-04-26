62e36071e74b57529440e006915de8ccd5156502
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-04-26 15:17:28 -0300

    Update Aldeano.php

--- Aldeano.php
+++ Aldeano.php
@@ -1,14 +1,27 @@
 <?php
+
     class Aldeano {
         
-        function __construct($nombre, $velrecoleccion){
+        function __construct($nombre, $velocidadrecoleccion = 19){
         $this->nombre = $nombre;
-        $this->velrecoleccion = $velrecoleccion;
+        $this->velocidadrecoleccion = $velocidadrecoleccion;
+    }
+        function getNombre($nombre){
+            $this->nombre = $nombre;
+        }
+        function SetNombre($set){
+            $this->nombre = $set;
+        }
+
+ 
+
+    function recolectar($arbusto){
+
     }
 
 }
 
-
-
+$leer = $_POST['nombrealdeano'];
+$Aldeano1 = new Aldeano ($leer);
 
 ?>
