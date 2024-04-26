6aa939f1b04e7c1eea7b8a2fec57d4c593fbecb7
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-04-26 16:03:11 -0300

    Update play.php

--- play.php
+++ play.php
@@ -1,7 +1,34 @@
 <?php
-    require_once('Aldeano.php')
-
-
-    
-
+require_once("Aldeano.php")
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Play</title>
+</head>
+<body>
+
+<img class="fondo" src="img/Fondo.png" alt="fondo">
+
+
+<?php
+    echo " 
+    <div class=cardAldeano>
+    <h2 id=titnombrealdeano>Nombre</h2>
+    <p id=nombrealdeano> ". $Aldeano1->nombre ."</p>
+    <h2 id=idtitvelocidad>Velocidad De Recoleccion</h2>
+    <p id=aldeanorecoleccion>".  $Aldeano1->velocidadrecoleccion ."</p>
+    </div>
+    ";
+?>
+
+
+</body>
+</html>
+
+
+
