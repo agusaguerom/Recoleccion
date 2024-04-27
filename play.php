6813b68e760a27177208495a5664ba2e6509afb8
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-04-27 14:58:55 -0300

    Update play.php

--- play.php
+++ play.php
@@ -8,22 +8,42 @@ require_once("Arbusto.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
+
 <body>
-    
-    <div class="row">
-        <div class="arbusto">
-            <img src="img/arbusto.png" alt="Arbusto imagen">
+    <div class="centrado">
+    <div class="container">
+    <div class="row sprites">
+        <div class="arbusto col">
+            <img src="img/Arbusto.png" alt="Arbusto imagen" class="arbustoimg">
         </div>
-        <div class="aldeano">
-            <img src="spritealdeano.png" alt="Aldeano Imagen">
+        <div class="aldeano col">
+            <img src="img/spritealdeano.png" alt="Aldeano Imagen">
+
+            <div class="datosaldeano">
+            <?php
+            
+            echo "<p class=velrecoleccion> Vel. Recoleccion " . $Aldeano1->velocidadrecoleccion . "</p>";
+            echo "<p class=nombrealdeano> Nombre </p>";
+            echo "<p class=nombrealdeano>". $Aldeano1->nombre ." </p>";
+
+            ?>
+            </div>
+
+
         </div>
-        <div class="arbusto">
-            <img src="img/Arbusto.png" alt="Arbusto imagen">
+        <div class="arbusto col">
+            <img src="img/Arbusto.png" alt="Arbusto imagen" class="arbustoimg">
         </div>
     </div>
+    </div>
+    </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 
