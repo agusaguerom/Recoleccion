1727d173206247a7017e09f87c1b09a061dd93aa
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-04-28 19:26:31 -0300

    Update Pesquero.php

--- Pesquero.php
+++ Pesquero.php
@@ -1,15 +1,18 @@
 <?php
 require_once("BancoDePesca.php");
 
+$nombrea = "Jose";
 if(isset($_POST['nombrepescador']) && $_POST['nombrepescador'] != ""){
     $nombrea = $_POST['nombrepescador'];
 }
 class Pesquero{
 
-    function __construct($nombre, $velocidadrecoleccion = 20, $totalrecolectado = 0){
+    function __construct($nombre = "Jose", $velocidadrecoleccion = 20, $totalrecolectado = 0, $imgpescador = 'img/Pescador1.gif'){
         $this->nombre = $nombre;
         $this->velocidadrecoleccion = $velocidadrecoleccion;
         $this->totalrecolectado = $totalrecolectado;
+        $this->imgpescador = $imgpescador;
+
 
     }
     function getNombre(){
@@ -30,11 +33,18 @@ class Pesquero{
     function SetTotalrecolectado($set){
         $this->totalrecolectado = $set;
     }
+    function getimgpescador(){
+        return $this->imgpescador;
+    }
+    function Setimgpescador($set){
+        $this->imgpescador = $set;
+    }
 
     function recolectar($BancoDePesca){
 
         if($BancoDePesca->getCantidadAlimento() > 0){
 
+            $this->Setimgpescador('img/Pescador2.gif');
             $tiemporecoleccion = ceil($BancoDePesca->getCantidadAlimento() / $this->velocidadrecoleccion) ;
             echo "<div class=contenedorrecolector>
             <h1 class=titulorecolectar> El Pescador recolecto todo en ". $tiemporecoleccion ." minutos<h1>
@@ -42,6 +52,12 @@ class Pesquero{
             </div>
             <h2 class=titulorecolectar>EL Banco de Pesca quedo vacio</h2>
             ";
+
+            $totalrecolectado = $this->getTotalrecolectado();
+            $totalrecolectado += $BancoDePesca->getCantidadAlimento();
+            $this->SetTotalrecolectado($totalrecolectado);
+    
+            $BancoDePesca->setCantidadAlimento(0);
         }
     }
 
