9b1bba48f05e3d5347afeaa9926a92ffc8a33cc9
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-04-28 16:34:08 -0300

    Update Aldeano.php

--- Aldeano.php
+++ Aldeano.php
@@ -1,10 +1,13 @@
 <?php
     require_once("Arbusto.php");
+    $leer = "";
     class Aldeano {
         
-        function __construct($nombre, $velocidadrecoleccion = 18){
+        function __construct($nombre="Pepe", $velocidadrecoleccion = 18, $totalrecolectado = 0){
         $this->nombre = $nombre;
         $this->velocidadrecoleccion = $velocidadrecoleccion;
+        $this->totalrecolectado = $totalrecolectado;
+
     }
         function getNombre(){
             return $this->nombre;
@@ -18,6 +21,12 @@
         function SetVelocidadRecoleccion($set){
             $this->velocidadrecoleccion = $set;
         }
+        function getTotalrecolectado(){
+            return $this->totalrecolectado;
+        }
+        function SetTotalrecolectado($set){
+            $this->totalrecolectado = $set;
+        }
         
 
     function recolectar($arbusto){
@@ -25,8 +34,11 @@
         echo "<h1> El aldeano recolecto todo en ". $tiemporecoleccion ." minutos<h1>";
     }
 
-}
+  
 
-$leer = $_POST['nombrealdeano'];
+}
+if(isset($_POST['nombrealdeano']) && ($_POST['nombrealdeano'] != "")) {
+    $leer = $_POST['nombrealdeano'];
+} 
 
 ?>
