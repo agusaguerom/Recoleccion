518b063b79195db5ac2d09146b6854c87616dd1d
agusaguerom <agustin.aguero@davinci.edu.ar>
2024-04-29 12:24:16 -0300

    Create Arbusto.php

---
+++ Clases/Arbusto.php
@@ -0,0 +1,38 @@
+<?php
+
+class Arbusto{
+
+    function __construct($cantidadAlimento = 125, $imgarbusto = 'img/Arbusto.png'){
+        $this->cantidadAlimento = $cantidadAlimento;
+        $this->imgarbusto = $imgarbusto;
+
+
+    }
+
+    function getAlimento(){
+        return $this->cantidadAlimento;
+    }
+    function setAlimento($set){
+        $this->cantidadAlimento = $set;
+    }
+    function getimgArbusto(){
+        return $this->imgarbusto;
+    }
+    function setimgArbusto($set){
+        $this->imgarbusto = $set;
+    }
+
+
+    function MostrarArbusto(){
+        echo 
+        "<div class=arbusto col>
+        <img src=img/Arbusto.png alt=Arbusto imagen class=arbustoimg>
+        <button class=recolectar>Recolectar</button>
+    </div>"
+        ;
+    }
+
+}
+
+
+?>
